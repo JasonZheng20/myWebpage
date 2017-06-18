df6f1249d13ab06ab345408bb6517fc6054483c3
Jason Zheng <jzzheng@stanford.edu>
2017-06-17 17:38:04 -0700

    taking a break

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 class App {
   constructor() {
     this.home = document.querySelector('.container#home');
+    this.title = document.querySelector('.title');
 
     this.meButton = document.querySelector('.button#me');
-    this.me = document.querySelector('.container#meC');
+    this.me = document.querySelector('.container3#meC');
 
     this.resumeButton = document.querySelector('.button#resume');
-    this.resume = document.querySelector('.container#resumeC');
+    this.resume = document.querySelector('.container3#resumeC');
 
     this.contactButton = document.querySelector('.button#contact');
-    this.contact = document.querySelector('.container#contactC');
+    this.contact = document.querySelector('.container3#contactC');
 
     this.showMe = this.showMe.bind(this);
     this.meButton.addEventListener('click', this.showMe);
@@ -22,33 +23,38 @@ class App {
   }
 
   showMe() {
+    this.title.textContent = "Me";
+    this.title.style = "font-size: 32px";
+    this.meButton.classList.add('highlighted');
+    this.resumeButton.classList.remove('highlighted');
+    this.contactButton.classList.remove('highlighted');
     this.home.style.transform = "translateY(-40vh)";
-    this.me.style.transform = "translateY(-77vh)";
-    this.resume.style.transform = "translateY(77vh)";
-    this.contact.style.transform = "translateY(77vh)";
-
-    // this.me.classList.remove('inactive');
-    // this.resume.classList.add('inactive');
-    // this.contact.classList.add('inactive');
+    this.me.style.transform = "translateY(-82vh)";
+    this.contact.style.transform = "translateY(82vh)";
+    this.resume.style.transform = "translateY(82vh)";
   }
 
   showResume() {
+    this.title.textContent = "Resume";
+    this.title.style = "font-size: 32px";
+    this.meButton.classList.remove('highlighted');
+    this.resumeButton.classList.add('highlighted');
+    this.contactButton.classList.remove('highlighted');
     this.home.style.transform = "translateY(-40vh)";
-    this.me.style.transform = "translateY(77vh)";
-    this.resume.style.transform = "translateY(-77vh)";
-    this.contact.style.transform = "translateY(77vh)";
-    // this.me.classList.add('inactive');
-    // this.resume.classList.remove('inactive');
-    // this.contact.classList.add('inactive');
+    this.me.style.transform = "translateY(82vh)";
+    this.resume.style.transform = "translateY(-82vh)";
+    this.contact.style.transform = "translateY(82vh)";
   }
 
   showContact() {
+    this.title.textContent = "Contact"
+    this.title.style = "font-size: 32px";
+    this.meButton.classList.remove('highlighted');
+    this.resumeButton.classList.remove('highlighted');
+    this.contactButton.classList.add('highlighted');
     this.home.style.transform = "translateY(-40vh)";
-    this.me.style.transform = "translateY(77vh)";
-    this.resume.style.transform = "translateY(77vh)";
-    this.contact.style.transform = "translateY(-77vh)";
-    // this.me.classList.add('inactive');
-    // this.resume.classList.add('inactive');
-    // this.contact.classList.remove('inactive');
+    this.me.style.transform = "translateY(82vh)";
+    this.resume.style.transform = "translateY(82vh)";
+    this.contact.style.transform = "translateY(-82vh)";
   }
 }
